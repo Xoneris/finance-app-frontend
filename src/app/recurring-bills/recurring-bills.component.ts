3c24f02126c5f88409e7ef5c9947b0c6497df04d
Xoneris <kely42@gmx.ch>
2024-09-14 06:21:40 +0200

    done most of the recurring-bills page

--- src/app/recurring-bills/recurring-bills.component.ts
+++ src/app/recurring-bills/recurring-bills.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import * as data from '../../../data.json'
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-recurring-bills',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, DatePipe],
   templateUrl: './recurring-bills.component.html',
   styleUrl: './recurring-bills.component.css'
 })
 export class RecurringBillsComponent {
-
+  recurringBills: any = (data.transactions.filter(bill => bill.recurring === true) as any);
 }
