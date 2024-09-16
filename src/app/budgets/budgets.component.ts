0bb3dc0c1e605bb8772406652939ed510ea87c90
Xoneris <kely42@gmx.ch>
2024-09-17 00:32:59 +0200

    finished most of budgets page

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -10,37 +10,20 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
   styleUrl: './budgets.component.css'
 })
 export class BudgetsComponent {
-
   budgets = data.budgets
   transactions = data.transactions
-
 
   getTotalSpent(budgetCategory:string) {
     const transactionsOfCategory = this.transactions.filter((transaction) => transaction.category === budgetCategory)
     const totalSpent = transactionsOfCategory.reduce((total, transaction) => total + transaction.amount, 0 )
-    return totalSpent
+    return Number(totalSpent.toString().replace("-",""))
   }
 
-  getRemaining(budgetCategory:string, budgetTotal:number) {
-    const transactionsOfCategory = this.transactions.filter((transaction) => transaction.category === budgetCategory)
-    const totalSpent = transactionsOfCategory.reduce((total, transaction) => total + transaction.amount, 0 )
-    return budgetTotal + totalSpent 
+  getRemaining(totalSpent:number, budgetTotal:number) {
+    return budgetTotal - totalSpent > 0 ? budgetTotal - totalSpent : 0
   }
 
   getTransactions(budgetCategory:string) {
     return this.transactions.filter((transaction) => transaction.category === budgetCategory).slice(0,3)
   }
-
 }
-
-const products = [
-  { name: 'Apple', price: 0.5, quantity: 10 },
-  { name: 'Banana', price: 0.25, quantity: 5 },
-  { name: 'Orange', price: 0.3, quantity: 8 }
-];
-
-const totalCost = products.reduce((accumulator, product) => {
-  return accumulator + product.price * product.quantity;
-}, 0);
-
-console.log(totalCost);
