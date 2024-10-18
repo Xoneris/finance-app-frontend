48bf4cc27e4949ad95a617e0fa6adf585d2d8660
Xoneris <kely42@gmx.ch>
2024-10-18 23:46:23 +0200

    started making the transactions page responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { NavigationComponent } from './navigation/navigation.component';
   template: `
     <div class="flex min-h-screen min-w-screen bg-[#F2F3F7]">
       <app-navigation></app-navigation>
-      <main class="grow my-6 mx-4 sm:my-8 sm:mx-10">
+      <main class="grow mt-6 mb-[98px] mx-4 sm:mt-8 sm:mb-[106px] sm:mx-10 lg:mb-8 ">
         <router-outlet></router-outlet>
       </main>
     </div>`
