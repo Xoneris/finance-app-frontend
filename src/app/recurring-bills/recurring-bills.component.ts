8e82d16aa2e46f4a68c9570e872fb0d05e40cdf1
Xoneris <kely42@gmx.ch>
2024-10-30 02:27:02 +0100

    search and sorting on recurring-bills page are now functional

--- src/app/recurring-bills/recurring-bills.component.ts
+++ src/app/recurring-bills/recurring-bills.component.ts
@@ -11,4 +11,98 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
 })
 export class RecurringBillsComponent {
   recurringBills: any = (data.transactions.filter(bill => bill.recurring === true) as any);
+  sortBy: string = ""
+  searchTerm: string = ""
+
+  updateSortBy(e:Event) {
+    const sortTarget = e.target as HTMLInputElement
+    this.sortBy = sortTarget.value
+  }
+
+  updateSearchTerm(e:Event) {
+    const searchTarget = e.target as HTMLInputElement
+    this.searchTerm = searchTarget.value
+  }
+
+  filteredRecurringBills() {
+    let filteredRecurringBills = this.recurringBills
+
+    switch(this.sortBy) {
+      case "latest":
+        filteredRecurringBills = filteredRecurringBills.sort(function(a:recurringBill,b:recurringBill){
+          if (a.date > b.date){
+            return -1 
+          } else if (a.date < b.date){
+            return 1
+          }
+          return 0
+        })
+        break
+      case "oldest":
+        filteredRecurringBills = filteredRecurringBills.sort(function(a:recurringBill,b:recurringBill){
+          if (a.date < b.date){
+            return -1 
+          } else if (a.date > b.date){
+            return 1
+          }
+          return 0
+        })
+        break
+      case "a-to-z":
+        filteredRecurringBills = filteredRecurringBills.sort(function(a:recurringBill,b:recurringBill){
+          if (a.name < b.name){
+            return -1 
+          } else if (a.name > b.name){
+            return 1
+          }
+          return 0
+        })
+        break
+      case "z-to-a":
+        filteredRecurringBills = filteredRecurringBills.sort(function(a:recurringBill,b:recurringBill){
+          if (a.name > b.name){
+            return -1 
+          } else if (a.name < b.name){
+            return 1
+          }
+          return 0
+        })
+        break
+      case "highest":
+        filteredRecurringBills = filteredRecurringBills.sort(function(a:recurringBill,b:recurringBill){
+          if (a.amount < b.amount){
+            return -1 
+          } else if (a.amount > b.amount){
+            return 1
+          }
+          return 0
+        })
+        break
+      case "lowest":
+        filteredRecurringBills = filteredRecurringBills.sort(function(a:recurringBill,b:recurringBill){
+          if (a.amount > b.amount){
+            return -1 
+          } else if (a.amount < b.amount){
+            return 1
+          }
+          return 0
+        })
+        break
+      default:
+        break
+    }
+    if (this.searchTerm !== "") {
+      filteredRecurringBills = filteredRecurringBills.filter((transaction:recurringBill) => transaction.name.toLowerCase().includes(this.searchTerm.toLowerCase()))
+    }
+    return filteredRecurringBills
+  }
+}
+
+interface recurringBill {
+  avatar: string,
+  name: string,
+  category: string,
+  date: string,
+  amount: number,
+  recurring: boolean,
 }
