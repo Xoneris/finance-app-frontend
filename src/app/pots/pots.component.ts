097605f27d8a2e8bef6af2e38567ccd40a171f50
Xoneris <kely42@gmx.ch>
2024-11-05 23:53:56 +0100

    added the edit/delete menu to the pots page, but without the dialog boxes

--- src/app/pots/pots.component.ts
+++ src/app/pots/pots.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import * as data from '../../../data.json'
-import { CurrencyPipe, DecimalPipe, NgStyle } from '@angular/common';
+import { CurrencyPipe, DecimalPipe, NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-pots',
   standalone: true,
-  imports: [CurrencyPipe, NgStyle, DecimalPipe],
+  imports: [CurrencyPipe, NgStyle, DecimalPipe, NgClass],
   templateUrl: './pots.component.html',
 })
 export class PotsComponent {
   pots: any = (data.pots as any);
+
+  showNewPotModal = false
+  showEditPotModal = false
+  showDeletePotModal = false
+
+  selectedPot = ''
+
+  openPotOptions(selectedPot:string) {
+    if (this.selectedPot === selectedPot) {
+      this.selectedPot = ''
+    } else {
+      this.selectedPot = selectedPot
+    }
+  }
+
+  openNewPotModal() {
+    this.showNewPotModal = true
+  }
+
+  openEditPotModal(selectedPot:string) {
+    this.showEditPotModal = true
+    this.selectedPot = selectedPot
+  }
+
+  openDeletePotModal(selectedPot:string) {
+    this.showDeletePotModal = true
+    this.selectedPot = selectedPot
+  }
 }
